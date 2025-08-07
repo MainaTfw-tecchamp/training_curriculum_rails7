b6ee0fc7e6a2907ceac0a68913fbbd1f367d9da3
MainaTfw-tecchamp <mainaTfw@gmail.com>
2025-08-07 21:24:58 +0900

    Issue6-2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,12 +1,11 @@
+require 'json'
 class CalendarsController < ApplicationController
 
-  # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    getweek
     @plan = Plan.new
   end
 
-  # 予定の保存
   def create
     Plan.create(plan_params)
     redirect_to action: :index
@@ -18,32 +17,40 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def getWeek
+  def getweek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
     @week_days = []
-
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
+      date = @todays_date + x
+      raw_plans = plans.select { |plan| plan.date == date }.map(&:plan)
+
       today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      raw_plans.each do |plan_string|
+        begin
+          parsed = JSON.parse(plan_string)
+          if parsed.is_a?(Array)
+            today_plans.concat(parsed)
+          else
+            today_plans.push(parsed.to_s)
+          end
+        rescue JSON::ParserError
+          today_plans.concat(plan_string.split(','))
+        end
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => (@wday_date+x).wday }
+
+      today_plans = today_plans.flatten.map(&:strip).uniq.reject(&:blank?)
+
+      wday_num = date.wday
+      days = {
+        month: date.month,
+        date: date.day,
+        plans: today_plans,
+        wday: wday_num
+      }
       @week_days.push(days)
     end
-
-    wday_num = (@todays_date + x).wday
-    if @wday_num >= 7 
-      wday_num = wday_num -7
-    end
-
-    days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
-    @week_days.push(days)
   end
 end
